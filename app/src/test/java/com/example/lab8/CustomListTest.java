1ed8a5b58dd8e56fd37f8377a520131f8bd10808
Manjot Saggu <msaggu@ualberta.ca>
2024-03-05 17:33:06 -0700

    Test case added

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,6 +11,26 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    /**
+     * this adds a city object to the list *for the first phase it will be empty * @param city
+     */
+    public void addCity(City city){
 
-
+    }
+    private CustomList list; /**
+     * create a mocklist for my citylist * @return
+     */
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>()); return list;
+    }
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size
+     plus one */
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount(); list.addCity(new City("Estevan", "SK")); assertEquals(list.getCount(),listSize + 1);
+    }
 }
